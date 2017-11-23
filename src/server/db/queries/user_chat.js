9431339688d0e67b3fbbe0ac55e0dd76be0ca396
Casey Siebel <leptone55@gmail.com>
2017-11-22 19:21:08 -0800

    Fix user test

--- src/server/db/queries/user_chat.js
+++ src/server/db/queries/user_chat.js
@@ -6,18 +6,11 @@ module.exports = {
     addUserChat: (user_id, chat_id) => userChats
                                         .insert({ user_id, chat_id })
                                         .returning('*'),
-    deleteUserChat: (user_id, chat_id) => {
-        
-        console.log('in query')
-        console.log('user_id', user_id)
-        console.log('chat_id', chat_id)
-
-        return userChats
+    deleteUserChat: (user_id, chat_id) => userChats
                                             .del()
                                             .where({ 
                                                 user_id: parseInt(user_id),
                                                 chat_id: parseInt(chat_id)
                                             })
                                             .returning('*')
-    }
 };
