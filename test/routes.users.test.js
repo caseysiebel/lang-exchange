31ea3379ba109f495dbad7c46c4966dafd43c780
Casey Siebel <leptone55@gmail.com>
2017-11-22 20:24:51 -0800

    Update num users value

--- test/routes.users.test.js
+++ test/routes.users.test.js
@@ -28,7 +28,7 @@ describe('routes : users', () => {
                     res.status.should.equal(200);
                     res.type.should.equal('application/json');
                     res.body.status.should.eql('success');
-                    res.body.data.length.should.eql(3);
+                    res.body.data.length.should.eql(4);
                     res.body.data[0].should.include.keys('id', 'username', 'password');
                     done();
                 });
