fd9a68beead3f3780904e46b052255d36a5a5bf5
Casey Siebel <leptone55@gmail.com>
2017-11-24 11:44:25 -0800

    Formate test file

--- test/routes.chats.test.js
+++ test/routes.chats.test.js
@@ -10,91 +10,91 @@ const knex = require('../src/server/db/connection');
 const user_chat_queries = require('../src/server/db/queries/user_chat');
 
 describe('routes : chats', () => {
-	beforeEach(() => {
-		return knex.migrate.rollback()
-			.then(() => knex.migrate.latest())
-			.then(() => knex.seed.run())
-	});
+    beforeEach(() => {
+        return knex.migrate.rollback()
+            .then(() => knex.migrate.latest())
+            .then(() => knex.seed.run())
+    });
 
-	afterEach(() => knex.migrate.rollback());
+    afterEach(() => knex.migrate.rollback());
 
-	after(() => server.close());
+    after(() => server.close());
 
-	describe('GET /api/v1/chats', () => {
-		it('should return all chats', (done) => {
-			chai.request(server)
-				.get('/api/v1/chats')
-				.end((err, res) => {
-					should.not.exist(err);
-					res.status.should.equal(200);
-					res.type.should.equal('application/json');
-					res.body.status.should.eql('success');
-					res.body.data.length.should.eql(2);
-					res.body.data[0].should.include.keys('id', 'created_at');
-					done();
-				})
-		});
-	}); 
-	describe('GET /api/v1/chats/:id', () => {
-		it('should return a single message', (done) => {
-			chai.request(server)
-				.get('/api/v1/chats/1')
-				.end((err, res) => {
-					should.not.exist(err);
-					res.status.should.equal(200);
-					res.type.should.equal('application/json');
-					res.body.status.should.eql('success');
-					res.body.data[0].should.include.keys('id', 'created_at');
-					done();
-				});
-		});
-		it('should throw an error if the chat does not exist', (done) => {
-			chai.request(server)
-				.get('/api/v1/chats/999999999')
-				.end((err, res) => {
-					should.exist(err);
-					res.status.should.equal(404);
-					res.type.should.equal('application/json');
-					res.body.status.should.eql('error');
-					res.body.message.should.eql('That chat does not exist.');
-					done();
-				});
-		});
-	});
-	describe('POST /api/v1/chat', () => {
-		it('should return the chat that was added', (done) => {
-			chai.request(server)
-				.post('/api/v1/chats')
-				.send({
+    describe('GET /api/v1/chats', () => {
+        it('should return all chats', (done) => {
+            chai.request(server)
+                .get('/api/v1/chats')
+                .end((err, res) => {
+                    should.not.exist(err);
+                    res.status.should.equal(200);
+                    res.type.should.equal('application/json');
+                    res.body.status.should.eql('success');
+                    res.body.data.length.should.eql(2);
+                    res.body.data[0].should.include.keys('id', 'created_at');
+                    done();
+                })
+        });
+    }); 
+    describe('GET /api/v1/chats/:id', () => {
+        it('should return a single message', (done) => {
+            chai.request(server)
+                .get('/api/v1/chats/1')
+                .end((err, res) => {
+                    should.not.exist(err);
+                    res.status.should.equal(200);
+                    res.type.should.equal('application/json');
+                    res.body.status.should.eql('success');
+                    res.body.data[0].should.include.keys('id', 'created_at');
+                    done();
+                });
+        });
+        it('should throw an error if the chat does not exist', (done) => {
+            chai.request(server)
+                .get('/api/v1/chats/999999999')
+                .end((err, res) => {
+                    should.exist(err);
+                    res.status.should.equal(404);
+                    res.type.should.equal('application/json');
+                    res.body.status.should.eql('error');
+                    res.body.message.should.eql('That chat does not exist.');
+                    done();
+                });
+        });
+    });
+    describe('POST /api/v1/chat', () => {
+        it('should return the chat that was added', (done) => {
+            chai.request(server)
+                .post('/api/v1/chats')
+                .send({
                     created_at: Date.now(),
                     user_ids: [ 2, 4 ]
-				})
-				.end((err, res) => {
-					should.not.exist(err);
-					res.status.should.equal(201);
-					res.type.should.equal('application/json');
-					res.body.status.should.eql('success');
+                })
+                .end((err, res) => {
+                    should.not.exist(err);
+                    res.status.should.equal(201);
+                    res.type.should.equal('application/json');
+                    res.body.status.should.eql('success');
                     const chat = res.body.data;
-					chat.should.include.keys('id', 'created_at');
-					done();
-				});
-		});
-		it('should add 2 user_chats', (done) => {
-			chai.request(server)
-				.post('/api/v1/chats')
-				.send({
+                    chat.should.include.keys('id', 'created_at');
+                    done();
+                });
+        });
+        it('should add 2 user_chats', (done) => {
+            chai.request(server)
+                .post('/api/v1/chats')
+                .send({
                     created_at: Date.now(),
                     user_ids: [ 2, 4 ]
-				})
-				.end((err, res) => {
-					should.not.exist(err);
-					res.status.should.equal(201);
-					res.type.should.equal('application/json');
-					res.body.status.should.eql('success');
+                })
+                .end((err, res) => {
+                    should.not.exist(err);
+                    res.status.should.equal(201);
+                    res.type.should.equal('application/json');
+                    res.body.status.should.eql('success');
                     const chat = res.body.data;
-					chat.should.include.keys('id', 'created_at');
+                    chat.should.include.keys('id', 'created_at');
                     let num_user_chats = 0;
-                        
+
                     console.log('&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&')
                     console.log('chat', chat)
                     console.log('chat.id', chat.id)
@@ -114,93 +114,93 @@ describe('routes : chats', () => {
                     done();
                 });
         });
-		it('should throw an error if the payload if malformed', (done) => {
-			chai.request(server)
-				.post('/api/v1/users')
-				.send({ username: 'jerry' })
-				.end((err, res) => {
-					should.exist(err);
-					res.status.should.equal(400);
-					res.type.should.equal('application/json');
-					res.body.status.should.eql('error');
-					should.exist(res.body.message);
-					done();
-				});
-		});
-	});
-	describe('PUT /api/v1/chats', () => {
-		it('should return the chat that was updated', (done) => {
-			knex('chats')
-				.select('*')
-				.then((chat) => {
-					const chatObject = chat[0];
-					chai.request(server)
-						.put(`/api/v1/chats/${chatObject.id}`)
-						.send({
-							created_at: Date.now()
-						})
-						.end((err, res) => {
-							should.not.exist(err);
-							res.status.should.equal(200);
-							res.type.should.equal('application/json');
-							res.body.status.should.eql('success');
-							res.body.data[0].should.include.keys('id', 'created_at');
-							const newChatObject = res.body.data[0];
+        it('should throw an error if the payload if malformed', (done) => {
+            chai.request(server)
+                .post('/api/v1/users')
+                .send({ username: 'jerry' })
+                .end((err, res) => {
+                    should.exist(err);
+                    res.status.should.equal(400);
+                    res.type.should.equal('application/json');
+                    res.body.status.should.eql('error');
+                    should.exist(res.body.message);
+                    done();
+                });
+        });
+    });
+    describe('PUT /api/v1/chats', () => {
+        it('should return the chat that was updated', (done) => {
+            knex('chats')
+                .select('*')
+                .then((chat) => {
+                    const chatObject = chat[0];
+                    chai.request(server)
+                        .put(`/api/v1/chats/${chatObject.id}`)
+                        .send({
+                            created_at: Date.now()
+                        })
+                        .end((err, res) => {
+                            should.not.exist(err);
+                            res.status.should.equal(200);
+                            res.type.should.equal('application/json');
+                            res.body.status.should.eql('success');
+                            res.body.data[0].should.include.keys('id', 'created_at');
+                            const newChatObject = res.body.data[0];
                             newChatObject.created_at.should.not.eql(chatObject.created_at);
-							done();
-						});
-				});
-		});
-		it('should throw an error if the chat does not exist', (done) => {
-			chai.request(server)
-				.put('/api/v1/chats/999999999')
-				.send({
-					created_at: Date.now()
-				})
-				.end((err, res) => {
-					should.exist(err);
-					res.status.should.equal(404);
-					res.type.should.equal('application/json');
-					res.body.status.should.eql('error');
-					res.body.message.should.eql('That chat does not exist.');
-					done();
-				});
-		});
-	});
-	describe('DELETE /api/v1/chats/:id', () => {
-		it('should return the chat that was deleted', (done) => {
-			knex('chats')
-				.select('*')
-				.then((chats) => {
-					const chatObject = chats[0];
-					const lengthBeforeDelete = chats.length;
-					chai.request(server)
-						.delete(`/api/v1/chats/${chatObject.id}`)
-						.end((err, res) => {
-							should.not.exist(err);
-							res.status.should.equal(200);
-							res.type.should.equal('application/json');
-							res.body.status.should.eql('success');
-							res.body.data[0].should.include.keys('id', 'created_at');
-							knex('chats').select('*')
-								.then((updatedChats) => {
-									updatedChats.length.should.eql(lengthBeforeDelete - 1);
-									done();
-								});
-						});
-				});
-		});
-		it('should throw an error if the chat does not exist', (done) => {
-			chai.request(server)
-				.delete('/api/v1/chats/999999999')
-				.end((err, res) => {
-					should.exist(err);
-					res.status.should.equal(404);
-					res.type.should.equal('application/json');
-					res.body.status.should.eql('error');
-					res.body.message.should.eql('That chat does not exist.');
-					done();
-				});
-		});
-	});
+                            done();
+                        });
+                });
+        });
+        it('should throw an error if the chat does not exist', (done) => {
+            chai.request(server)
+                .put('/api/v1/chats/999999999')
+                .send({
+                    created_at: Date.now()
+                })
+                .end((err, res) => {
+                    should.exist(err);
+                    res.status.should.equal(404);
+                    res.type.should.equal('application/json');
+                    res.body.status.should.eql('error');
+                    res.body.message.should.eql('That chat does not exist.');
+                    done();
+                });
+        });
+    });
+    describe('DELETE /api/v1/chats/:id', () => {
+        it('should return the chat that was deleted', (done) => {
+            knex('chats')
+                .select('*')
+                .then((chats) => {
+                    const chatObject = chats[0];
+                    const lengthBeforeDelete = chats.length;
+                    chai.request(server)
+                        .delete(`/api/v1/chats/${chatObject.id}`)
+                        .end((err, res) => {
+                            should.not.exist(err);
+                            res.status.should.equal(200);
+                            res.type.should.equal('application/json');
+                            res.body.status.should.eql('success');
+                            res.body.data[0].should.include.keys('id', 'created_at');
+                            knex('chats').select('*')
+                                .then((updatedChats) => {
+                                    updatedChats.length.should.eql(lengthBeforeDelete - 1);
+                                    done();
+                                });
+                        });
+                });
+        });
+        it('should throw an error if the chat does not exist', (done) => {
+            chai.request(server)
+                .delete('/api/v1/chats/999999999')
+                .end((err, res) => {
+                    should.exist(err);
+                    res.status.should.equal(404);
+                    res.type.should.equal('application/json');
+                    res.body.status.should.eql('error');
+                    res.body.message.should.eql('That chat does not exist.');
+                    done();
+                });
+        });
+    });
 });
