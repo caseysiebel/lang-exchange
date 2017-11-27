6b9407eaeddc0a38ebb1cc421796e2c29985e6fc
Casey Siebel <leptone55@gmail.com>
2017-11-27 15:08:48 -0800

    Chai use chai-http

--- test/routes.chats.test.js
+++ test/routes.chats.test.js
@@ -3,6 +3,7 @@ process.NODE_ENV = 'test';
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
 
 const server = require('../src/server/index');
 const knex = require('../src/server/db/connection');
