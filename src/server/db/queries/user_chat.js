78963765a647f7e166df40ec38e96d71d4569f9e
Casey Siebel <leptone55@gmail.com>
2017-11-29 09:14:52 -0800

    Correct deleteUserChat query

--- src/server/db/queries/user_chat.js
+++ src/server/db/queries/user_chat.js
@@ -8,7 +8,7 @@ const queries = {
         return user_chat;
     }),
     deleteUserChat: (user_id, chat_id) => {
-        return userChats
+        return db('user_chat')
             .del()
             .where({ 
                 user_id: parseInt(user_id),
